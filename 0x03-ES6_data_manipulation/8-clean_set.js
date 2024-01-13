25362790a9e6348b2d1b92b7981a929df18ae24d
ghaidaareda <dodo.sa7afy@gmail.com>
2024-01-13 20:45:15 +0200

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSety(set, startString) {
-  if (startString.length > 0) {
-    const arraySet = [...set];
-    const slicedarray = arraySet
-      .filter((item) => item.startsWith(startString))
-      .map((item) => item.slice(startString.length));
-    return slicedarray.join('-');
-  }
-  set.clear();
+if (startString.length > 0){
+const arraySet = [...set];
+const slicedarray = arraySet
+    .filter(item => item.startsWith(startString))
+    .map(item => item.slice(startString.length));
+return slicedarray.join('-');
+} else {
   return '';
+}
 }
